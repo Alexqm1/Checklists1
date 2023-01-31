fe0862c2bda4d379ca06fee9d83f696a30ff1a67
Alexqm1 <105534648+Alexqm1@users.noreply.github.com>
2023-01-30 23:39:56 -0500

    Initial commit

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Alex Quezada Magdaleno on 1/29/23.
+//
+
+import Foundation
+
+class ChecklistItem: NSObject {
+  var text = ""
+  var checked = false
+}
